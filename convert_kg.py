e6accddc167d1f01212262ef9d054c61b6256bd6
Alex <alex@TUs-iMac.local>
2020-07-03 09:25:34 +1000

    complete

--- convert_kg.py
+++ convert_kg.py
@@ -2,13 +2,27 @@ from tkinter import *
 
 window = Tk()
 
+def convert_all():
+    grams.delete('1.0',END)
+    pounds.delete('1.0',END)
+    ounces.delete('1.0',END)
+    value = float(user_value.get())
+    g = value * 1000
+    p = value * 2.205
+    o = value * 35.274
+    grams.insert(END,g)
+    pounds.insert(END,p)
+    ounces.insert(END,o)
+
+
 kg = Label(window,text='kg')
 kg.grid(row=0,column=0)
 
-user_input = Entry(window)
+user_value = StringVar()
+user_input = Entry(window,textvariable=user_value)
 user_input.grid(row=0,column=1)
 
-bt = Button(window,text='Convert')
+bt = Button(window,text='Convert', command=convert_all)
 bt.grid(row=0,column=3)
 
 grams = Text(window,height=1,width=20,borderwidth=2,relief="groove")
